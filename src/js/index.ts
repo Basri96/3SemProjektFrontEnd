7fca6e7af1e5d26ba681b45d1324702d588bb2e9
Hasan Al-Basri <hasa0173@edu.easj.dk>
2019-05-13 14:04:12 +0200

    added year

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,9 +6,11 @@ import {weight} from "./weight"
 let divElement : HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 let idagBtn:HTMLButtonElement = <HTMLButtonElement> document.getElementById("idagBtn");
 let ugeBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ugeBtn");
-let Månedvalg = <>document.getElementById("Månedvalg")
-ugeBtn.addEventListener('click', plotUge)
+let årBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("årBtn");
+let Månedvalg = <HTMLButtonElement>document.getElementById("Månedvalg")
 idagBtn.addEventListener('click', plotIdag);
+ugeBtn.addEventListener('click', plotUge)
+årBtn.addEventListener('click', plotÅr)
 
 function MånedMuligehder(){
     if(Månedvalg.value == "Måned"){
@@ -32,7 +34,7 @@ function plotIdag():void{
         let result: string = "<ul>"
         
         response.data.forEach((weight: weight) => {
-            result += "<li>"+weight.id+" "+weight.dato+" "+weight.weightMeasure+"</li>"    
+            result += "<li>"+weight.dato+" "+weight.weightMeasure+"</li>"    
         });
         result +="</ul>"
 
@@ -51,6 +53,32 @@ function plotIdag():void{
 function plotUge():void{
 
     axios.get<weight[]>("https://localhost:44355/api/weight/2")
+    .then(function(response: AxiosResponse<weight[]>): void
+    {
+        console.log(response);
+
+        let result: string = "<ul>"
+        
+        response.data.forEach((weight: weight) => {
+            result += "<li> "+weight.weightMeasure+" "+weight.dato+" "+weight.id+"</li>"    
+        });
+        result +="</ul>"
+
+        divElement.innerHTML = result;
+    })
+    .catch(
+        function(error: AxiosError ): void{
+            console.log("errrrrrror in my code")
+            console.log(error);
+        }
+        
+    )   
+    console.log("er i slutning af getAllCustomers function");
+}
+
+function plotÅr():void{
+
+    axios.get<weight[]>("https://localhost:44355/api/weight/4")
     .then(function(response: AxiosResponse<weight[]>): void
     {
         console.log(response);
