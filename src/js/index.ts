2939ec8b5b9d97c8201a97492ec90a3040025735
felex <felicxmose@msn.com>
2019-05-14 13:51:29 +0200

    sprint done done

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,12 +7,21 @@ let divElement : HTMLDivElement = <HTMLDivElement> document.getElementById("cont
 let idagBtn:HTMLButtonElement = <HTMLButtonElement> document.getElementById("idagBtn");
 let ugeBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ugeBtn");
 let årBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("årBtn");
-let Månedvalg = <HTMLButtonElement>document.getElementById("Månedvalg")
+let månedsGennemsnitBtn = <HTMLButtonElement>document.getElementById("månedsGennemsnitBtn");
+let Månedvalg = <HTMLButtonElement>document.getElementById("Månedvalg");
+
 /** let Månedvalg = <>document.getElementById("Månedvalg") */
-ugeBtn.addEventListener('click', plotUge)
+
 idagBtn.addEventListener('click', plotIdag);
 ugeBtn.addEventListener('click', plotUge)
 årBtn.addEventListener('click', plotÅr)
+månedsGennemsnitBtn.addEventListener('click', månedsGennemsnit);
+
+/*
+let basket: number[] = [2,2,2,2];
+let sum1 = basket.reduce((a, b) => a + b, 0);
+console.log(sum1/basket.length);
+*/
 
 /*
 function MånedMuligehder(){
@@ -79,6 +88,36 @@ function plotUge():void{
     console.log("er i slutning af getAllCustomers function");
 }
 
+function månedsGennemsnit():void{
+
+    axios.get<weight[]>("https://localhost:44355/api/weight/3")
+    .then(function(response: AxiosResponse<weight[]>): void
+    {
+        console.log(response);
+        console.log("r i then");
+        let result: number[] = new Array;
+
+        response.data.forEach((weight: weight) => {
+            let gram = Number(weight.dato);
+            result.push(gram) 
+        });
+        /*gå igennem result listen og lægger tallene sammen */
+       let sum = result.reduce((a, b) => a + b, 0)
+       
+       let myÅrMåned = (response.data[0].weightMeasure).substring(0,7);
+        
+        divElement.innerHTML = "Måned: "+myÅrMåned+" "+"Gennemsnits Madspild: "+String(sum/result.length);
+    })
+    .catch(
+        function(error: AxiosError ): void{
+            console.log("errrrrrror in my code")
+            console.log(error);
+        }
+        
+    )   
+    console.log("er i slutning af getAllCustomers function");
+}
+
 function plotÅr():void{
 
     axios.get<weight[]>("https://localhost:44355/api/weight/4")
@@ -89,7 +128,7 @@ function plotÅr():void{
         let result: string = "<ul>"
         
         response.data.forEach((weight: weight) => {
-            result += "<li>"+weight.id+" "+weight.dato+" "+weight.weightMeasure+"</li>"    
+            result += "<li>"+"Dato:"+" "+weight.weightMeasure+" "+"Weight: "+""+weight.dato+""+"</li>"    
         });
         result +="</ul>"
 
